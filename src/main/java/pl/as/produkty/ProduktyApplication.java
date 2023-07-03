279c64f8d41cae1bee1772c2e8fd48a4553698c4
rbzf <anna.sadkowska@gmail.com>
2023-07-03 07:39:25 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/as/produkty/ProduktyApplication.java
@@ -0,0 +1,13 @@
+package pl.as.produkty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduktyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProduktyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/as/produkty/ProduktyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.as.produkty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduktyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
